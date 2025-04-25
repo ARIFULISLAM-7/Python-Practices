835ddb3884e224ee281142014758c9c7ee8a8a0a
Ariful Islam <ariful.pacs@.gmail.com>
2025-04-25 23:30:55 +0600

    python variables

---
+++ variable.py
@@ -0,0 +1,31 @@
+# Variable = A container for a value (string, integer, float, boolean)
+# A variable behaves as if it was the value it contains.
+
+# strings
+first_name = "Ariful"
+last_name = "Islam"
+print(f"My name is {first_name} {last_name}.")
+
+# integers
+age = 22
+print(f"I am {age} years old.")
+
+# float
+height = 5.5
+print(f"My height is {height} feet.")
+
+# boolean
+is_student = False
+print(f"Am I a student? {is_student}")
+
+if is_student:
+    print("I am a student.")
+else:
+    print("I am not a student.")
+
+for_sale = True
+
+if for_sale:
+    print("This item is for sale.")
+else:
+    print("This item is not for sale.")
