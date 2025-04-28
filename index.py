b1aaaa3fd0f0541fccf01b7e03b221f848e89ef2
Ariful Islam <ariful.pacs@.gmail.com>
2025-04-28 09:50:25 +0600

    modified- version check

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
 # This is a simple Python script that prints a message to the console.
 # It demonstrates the use of the print function.
 print("I am number seven!")
+
+import sys
+print(sys.version)
